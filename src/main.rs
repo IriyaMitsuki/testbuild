fe6dc37734165e254b0a616e9223132cadb658d4
IriyaMitsuki <iriyamitsuki@gmail.com>
2025-05-22 16:46:40 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
 use std::fmt::Write;
 use std::io::{self, Write as IoWrite};
 use std::time::{Duration, Instant};
+use std::ptr;
+
+#[cfg(target_os = "windows")]
+use windows_sys::Win32::{
+    System::Console::SetConsoleTitleW,
+    Globalization::{MultiByteToWideChar, CP_UTF8},
+};
+
+#[cfg(target_os = "windows")]
+fn set_console_title(title: &str) {
+    let mut wide_chars: Vec<u16> = title.encode_utf16().collect();
+    wide_chars.push(0);
+    unsafe {
+        SetConsoleTitleW(wide_chars.as_ptr());
+    }
+}
+
+#[cfg(not(target_os = "windows"))]
+fn set_console_title(_title: &str) {
+}
+
 
 fn main() {
+    set_console_title("Rust Ultimate Overlord v0.70b");
+
     let ascii_art = vec![
         r#"  _____           _     _   _______          _       _ "#,
         r#" |  __ \         | |   | | |__   __|        | |     | |"#,
@@ -14,11 +37,11 @@ fn main() {
     ];
 
     let colors = [
-        (255, 0, 0), 
+        (255, 0, 0),
         (255, 127, 0),
-        (255, 255, 0), 
-        (0, 255, 0), 
-        (0, 0, 255), 
+        (255, 255, 0),
+        (0, 255, 0),
+        (0, 0, 255),
         (75, 0, 130),
         (148, 0, 211),
     ];
@@ -31,14 +54,13 @@ fn main() {
     println!("\x1b[0m");
     io::stdout().flush().unwrap();
 
-    std::thread::sleep(Duration::from_secs(25));
-
+    std::thread::sleep(Duration::from_secs(5));
     print!("\x1b[2J\x1b[H");
     io::stdout().flush().unwrap();
 
     let bar_length = 50;
     let start_time = Instant::now();
-    let total_duration = Duration::from_secs(45);
+    let total_duration = Duration::from_secs(15);
 
     loop {
         let elapsed = start_time.elapsed();
@@ -53,9 +75,8 @@ fn main() {
 
         for i in 0..bar_length {
             if i < filled_chars {
-                // Исправленные вычисления цвета
-                let r = (255.0 * (1.0 - (i as f32 / (bar_length - 1) as f32))) as u8;
-                let g = (255.0 * (i as f32 / (bar_length - 1) as f32)) as u8;
+                let r = (255.0 * (1.0 - (i as f32 / (bar_length -1).max(1) as f32))) as u8;
+                let g = (255.0 * (i as f32 / (bar_length -1).max(1) as f32)) as u8;
                 let b = 0;
                 write!(&mut bar, "\x1b[38;2;{};{};{}m█", r, g, b).unwrap();
             } else {
@@ -68,6 +89,7 @@ fn main() {
         io::stdout().flush().unwrap();
 
         if progress >= 100.0 {
+            println!("\x1b[0m");
             break;
         }
 
@@ -75,8 +97,8 @@ fn main() {
     }
 
     print!("\x1b[2J\x1b[H");
-    println!("\x1b[32mActivation successful! Closing console in 25 seconds...\x1b[0m");
+    println!("\x1b[32mActivation successful! Closing console in 5 seconds...\x1b[0m");
     io::stdout().flush().unwrap();
 
-    std::thread::sleep(Duration::from_secs(25));
+    std::thread::sleep(Duration::from_secs(5));
 }
